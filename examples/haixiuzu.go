5db1147b218aa5bb597ac31964aa39933f632fec
mnhkahn <lichao0407@gmail.com>
2015-03-21 21:59:21 +0800

    add example

---
+++ examples/haixiuzu.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/mnhkahn/maodou"
+	"github.com/mnhkahn/maodou/dao"
+	"github.com/mnhkahn/maodou/models"
+	"strings"
+)
+
+type Haixiu struct {
+	maodou.MaoDou
+}
+
+func (this *Haixiu) Start() {
+	this.Index(this.Cawl("http://www.douban.com/group/haixiuzu/discussion"))
+}
+
+func (this *Haixiu) Index(resp *maodou.Response) {
+	resp.Doc(`#content > div > div.article > div:nth-child(2) > table > tbody > tr > td.title > a`).Each(func(i int, s *goquery.Selection) {
+		href, has := s.Attr("href")
+		if has {
+			if href == "" || strings.HasPrefix(href, "/") {
+				href = "http://localhost:4000" + href + "/"
+			}
+			this.Detail(this.Cawl(href))
+		}
+	})
+}
+
+func (this *Haixiu) Detail(resp *maodou.Response) {
+	res := new(models.Result)
+	res.Title = resp.Doc("#content > h1").Text()
+	res.Author = resp.Doc("#content > div > div.article > div.topic-content.clearfix > div.topic-doc > h3 > span.from > a").Text()
+	res.Figure, _ = resp.Doc("#link-report > div.topic-content > div.topic-figure.cc > img").Attr("src")
+	this.Result(res)
+}
+
+func (this *Haixiu) Result(result *models.Result) {
+	if result.Figure != "" {
+		Dao, err := dao.NewDao("solr", "http://127.0.0.1:8983/solr/post")
+		if err != nil {
+			panic(err)
+		}
+		Dao.AddResult(result)
+	}
+}
+
+func main() {
+	maodou.APP.Register(new(Haixiu))
+}
