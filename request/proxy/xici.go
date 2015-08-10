468e543e14b58b587015fff77c81587a5920c45a
lichao <lichao@rd.tuan800.com>
2015-08-10 18:05:52 +0800

    sync

--- request/proxy/xici.go
+++ request/proxy/xici.go
@@ -128,7 +128,7 @@ func (this *XiciProxyContainer) TestProxy(p *ProxyConfig) bool {
 	u := new(urlpkg.URL)
 	u.Scheme = "http"
 	u.Host = fmt.Sprintf("%s:%d", p.Ip, p.Port)
-	res, err := goreq.Request{Uri: this.config.Root, Proxy: u.String(), Timeout: time.Duration(10) * time.Second}.Do()
+	res, err := goreq.Request{Uri: this.config.Root, Proxy: u.String(), Timeout: time.Duration(5) * time.Second}.Do()
 	if err != nil && res != nil && res.StatusCode == http.StatusOK {
 		return false
 	}
