1eb79b2e3b5130cbf1810f863295d43b19baaa13
lichao <lichao@rd.tuan800.com>
2015-08-11 10:31:09 +0800

    update utils

--- utils/test_proxy.go
+++ utils/test_proxy.go
@@ -10,6 +10,7 @@ import (
 )
 
 var host = flag.String("h", "host", "")
+var url = flag.String("u", "http://www.douban.com/group/topic/78216948/", "")
 
 func main() {
 	goreq.SetConnectTimeout(5 * time.Second)
@@ -19,6 +20,6 @@ func main() {
 	u := new(urlpkg.URL)
 	u.Scheme = "http"
 	u.Host = *host
-	res, err := goreq.Request{Uri: "http://www.douban.com/group/topic/78216948/", Proxy: u.String(), Timeout: time.Duration(5) * time.Second, ShowDebug: true}.Do()
+	res, err := goreq.Request{Uri: *url, Proxy: u.String(), Timeout: time.Duration(5) * time.Second, ShowDebug: true}.Do()
 	log.Println(res.Status, err)
 }
